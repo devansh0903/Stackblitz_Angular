d107810e693f60b756db1cd6309d554600d7a900
devansh0903 <devanshn@buffalo.edu>
2022-03-25 12:25:28 +0800

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { DesignComponent } from './design/design.component';
+import { ExerciseComponent } from './exercise/exercise.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, DesignComponent ],
-  bootstrap:    [ AppComponent, DesignComponent ]
+  declarations: [ AppComponent, HelloComponent, DesignComponent,ExerciseComponent ],
+  bootstrap:    [ AppComponent, DesignComponent, ExerciseComponent ]
 })
 export class AppModule { }
